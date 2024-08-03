50e57f6aec77d5a742c8588321743ac39e8142e6
Donaw <177387755+Don-aw@users.noreply.github.com>
2024-08-03 22:16:59 +0800

    Remove unused dependencies

--- src/main/java/me/don/hypixelapitest/attributeInfo.java
+++ src/main/java/me/don/hypixelapitest/attributeInfo.java
@@ -2,9 +2,6 @@ package me.don.hypixelapitest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.ArrayList;
-import java.util.Objects;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record attributeInfo(int startingBid, Boolean bin) {
 
